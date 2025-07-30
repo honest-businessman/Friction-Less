727304954740c93c6ec3df5555a92d682474b914
honest-businessman <2024000429@sit.ac.nz>
2025-07-31 01:28:39 +1200

    Tank movement is now physics based with slight drag applied to movement. Added gamepad visualizer.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
-    [SerializeField] private float pMoveSpeed = 3f;
-    [SerializeField] private float pRotSpeed = 40f;
+    public float pMoveSpeed = 25f; // Movement speed
+    private float pMoveSpeedMultiplier = 100f; // Adjusted multiplier for movement speed
+    public float pRotSpeed = 10f; // Rotation speed
+    private float pRotSpeedMultiplier = 10f; // Adjusted multiplier for rotation speed
+    public float treadOffset = 0.2f; // Offset for the rotation pivot point
+    private float debugOffset = 2f; // Offset for the debug line length
     private Vector2 playerInput;
     private Vector3 velocity;
     private Rigidbody rb;
@@ -21,15 +25,36 @@ public class PlayerController : MonoBehaviour
         playerInput = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
         PlayerMovement();
     }
-
     private void PlayerMovement()
     {
-        controller.Move(transform.up * playerInput.y * pMoveSpeed * Time.deltaTime);
-        transform.Rotate(transform.forward * -playerInput.x * pRotSpeed * Time.deltaTime);
+        Debug.DrawRay(transform.position, transform.up * 2, Color.green); // Draw ray pointing up from the player
+        rb.AddForce(transform.up * playerInput.y * (1 - Mathf.Abs(playerInput.x)) * pMoveSpeed * pMoveSpeedMultiplier * Time.deltaTime, ForceMode.Force);
+        PlayerRotate();
     }
+    private void PlayerRotate()
+    {
+        Vector3 rotationPoint = transform.position; // Point to rotate around
+        Vector3 debugPoint = transform.position; // Point to draw debug line to
+        if (Mathf.Abs(playerInput.y) > 0.1)
+        {
+            if (playerInput.x < 0)
+            {
+                rotationPoint += -transform.right * treadOffset;
+                debugPoint += -transform.right * treadOffset * debugOffset;
+            }
+            else if (playerInput.x > 0)
+            {
+                rotationPoint += transform.right * treadOffset;
+                debugPoint += transform.right * treadOffset * debugOffset;
+            }
+        }
+        Debug.DrawLine(transform.position, debugPoint, Color.blue); // Draw line roughly pointing to pivot point
+        transform.RotateAround(rotationPoint, transform.forward, -playerInput.x * pRotSpeed * pRotSpeedMultiplier * Time.deltaTime);
+    }
+
 }
