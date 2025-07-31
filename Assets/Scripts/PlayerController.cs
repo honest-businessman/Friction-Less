d0660ba663411cf578845cb48c7496cdcaa4e370
honest-businessman <matchil2plus3@gmail.com>
2025-07-31 15:00:09 +1200

    Added friction drive charging mechanic and drive charge meter ui element

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,8 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,15 +13,26 @@ public class PlayerController : MonoBehaviour
     public float defaultDrag = 8f;
     public float rotationSpeed = 15f; // Rotation speed
     private float moveSpeedMultiplier = 100f; // Adjusted multiplier for movement speed
-    public float driftTurnStrength = 2f; // Strength of the drift turn
     public float treadOffset = 0.2f; // Offset for the rotation pivot point
+    public float chargePerSecond = 60f;
+    public float dischargePerSecond = 70f;
+    public float chargeFadeDelay = 2f; // Delay before charge starts fading
+    public float chargeAngle = 45f; // degrees
+    public float minChargeMoveSpeed= 3f; // degrees
     private float rotationSpeedMultiplier = 10f; // Adjusted multiplier for rotation speed
-    private float debugOffset = 2f; // Offset for the debug line length
+    public float driveCharge = 0f;
 
-    private bool isDrifting = false; // Flag to check if the player is drifting
+    private enum MoveState
+    {
+        Idle,
+        Moving,
+        Drifting
+    }
+    private MoveState currentState = MoveState.Idle;
     private Vector2 playerInput;
-    private Vector3 velocity;
+    public bool driftPressed;
     private Rigidbody rb;
+    private float timeLastCharge;
 
     void Awake()
     {
@@ -30,16 +44,26 @@ public class PlayerController : MonoBehaviour
     private void OnMove(InputValue value)
     {
         playerInput = value.Get<Vector2>();
+        
     }
 
     private void OnDrift(InputValue value)
     {
-        isDrifting = value.isPressed;
+        driftPressed = value.isPressed;
+    }
+    private void ChageMoveState()
+    {
+        if (driftPressed)
+            currentState = MoveState.Drifting;
+        else if (playerInput.magnitude > 0.1f)
+            currentState = MoveState.Moving;
+        else
+            currentState = MoveState.Idle;
     }
 
     void FixedUpdate()
     {
-        if (isDrifting)
+        if (currentState == MoveState.Drifting)
         {
             rb.linearDamping = 0f; // Reduce linear damping when drifting
         }
@@ -52,16 +76,24 @@ public class PlayerController : MonoBehaviour
     }
     private void PlayerMovement()
     {
+        ChageMoveState();
+
         Debug.DrawRay(transform.position, transform.up * 2, Color.green); // Draw ray pointing up from the player
-        if (!isDrifting)
+        if (currentState == MoveState.Moving)
+        {
             rb.AddForce(transform.up * playerInput.y * (1.5f - Mathf.Abs(playerInput.x)) * moveSpeed * moveSpeedMultiplier * Time.deltaTime, ForceMode.Force);
-        else
+            FadeDrive();
+        }
+        else if (currentState == MoveState.Drifting)
         {
             rb.AddForce(transform.up * playerInput.y * driftSpeed * moveSpeedMultiplier * Time.deltaTime, ForceMode.Force);
-            
-            //DriftMovement();
+            ChargeDrive();
         }
+        else if (currentState == MoveState.Idle)
+            FadeDrive();
+
         PlayerRotate();
+
 
         // Clamp the velocity to the maximum speed
         if (rb.linearVelocity.magnitude > maxSpeed)
@@ -78,40 +110,45 @@ public class PlayerController : MonoBehaviour
             if (playerInput.x < 0)
             {
                 rotationPoint += -transform.right * treadOffset;
-                debugPoint += -transform.right * treadOffset * debugOffset;
             }
             else if (playerInput.x > 0)
             {
                 rotationPoint += transform.right * treadOffset;
-                debugPoint += transform.right * treadOffset * debugOffset;
             }
         }
-        Debug.DrawLine(transform.position, debugPoint, Color.blue); // Draw line roughly pointing to pivot point
         transform.RotateAround(rotationPoint, transform.forward, -playerInput.x * rotationSpeed * rotationSpeedMultiplier * Time.deltaTime);
     }
 
-    /*private void DriftMovement()
+    private void ChargeDrive()
     {
-        if (Mathf.Abs(playerInput.y) > 0.01f)
+        
+        Vector3 velocity = rb.linearVelocity;
+        float mag = velocity.magnitude;
+        Vector3 up = transform.up;
+
+        if (mag > minChargeMoveSpeed)
         {
-            Vector3 currentVelocity = rb.linearVelocity;
-            float speed = currentVelocity.magnitude;
-            Vector3 desiredDirection = transform.up * Mathf.Sign(playerInput.y);
-
-            // Calculate the max angle to rotate this frame (in radians)
-            float maxRadiansDelta = Mathf.Deg2Rad * driftTurnStrength; // Adjust 5f for drift responsiveness
-
-            // Rotate velocity direction toward desired direction
-            Vector3 newDirection = Vector3.RotateTowards(
-                currentVelocity.normalized,
-                desiredDirection,
-                maxRadiansDelta,
-                0f
-            );
-
-            rb.linearVelocity = newDirection * speed;
+            float fAngle = Vector3.Angle(up, velocity);
+            float bAngle = Vector3.Angle(-up, velocity);
+            
+            Debug.DrawRay(transform.position, velocity * 2, Color.red);
+            if (fAngle > chargeAngle && bAngle > chargeAngle)
+            {
+                if (driveCharge >= 100f)
+                    driveCharge = 100f; // Cap the charge at 100%
+                else
+                    driveCharge += (chargePerSecond * Time.deltaTime) * (mag/maxSpeed) * Mathf.Abs(playerInput.y);
+                timeLastCharge = Time.time; // Reset the charge fade timer
+            }
         }
-    }*/
-    
+    }
+
+    private void FadeDrive()
+    {
+        if (Time.time - timeLastCharge > chargeFadeDelay && driveCharge > 0f)
+            driveCharge -= (dischargePerSecond * Time.deltaTime) * Mathf.Clamp01((Time.time - timeLastCharge) / chargeFadeDelay);
+        else
+            driveCharge = Mathf.Max(driveCharge, 0f); // Ensure charge doesn't go below 0
+    }
 
 }
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+
+    public TextMeshProUGUI meterText;
+    public PlayerController playerController;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        meterText.text = $"Friction Charge: 0%";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayCharge();
+    }
+
+    public void DisplayCharge()
+    {
+        meterText.text = ($"Friction Charge: {playerController.driveCharge:0}%");
+    }
+}
