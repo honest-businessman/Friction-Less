c837a611360c8cc6b2b84086c53b2a9b67d75cd8
Keisuke-Suzuki229 <154766861+Keisuke-Suzuki229@users.noreply.github.com>
2025-08-07 18:26:03 +1200

    enemy

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
     public GameObject enemy1;
+    public GameObject enemy2;
+
     private int frameCount = 0;
     
 
@@ -20,8 +22,10 @@ public class SpawnEnemy : MonoBehaviour
 
         if(frameCount % 200 == 0)
         {
-            int currentEnemyCount = GameObject.FindGameObjectsWithTag("Enemy2").Length;
-            if(currentEnemyCount < 10)
+            int currentEnemy1Count = GameObject.FindGameObjectsWithTag("Enemy").Length;
+            int currentEnemy2Count = GameObject.FindGameObjectsWithTag("Enemy2").Length;
+
+            if (currentEnemy1Count < 5)
             {
                 Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
                 Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
@@ -42,7 +46,29 @@ public class SpawnEnemy : MonoBehaviour
                 Instantiate(enemy1, spawnPosition, Quaternion.identity);
 
             }
-            
+
+            if (currentEnemy2Count < 5)
+            {
+                Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+                Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+                SpriteRenderer sr = enemy2.GetComponent<SpriteRenderer>();
+                float enemyWidth = sr.bounds.size.x;
+                float enemyHeight = sr.bounds.size.y;
+
+                float minX = bottomLeft.x + enemyWidth / 2f;
+                float maxX = topRight.x - enemyWidth / 2f;
+                float minY = bottomLeft.y + enemyHeight / 2f;
+                float maxY = topRight.y - enemyHeight / 2f;
+
+                float x = Random.Range(minX, maxX);
+                float y = Random.Range(minY, maxY);
+
+                Vector3 spawnPosition = new Vector3(x, y, 0);
+                Instantiate(enemy2, spawnPosition, Quaternion.identity);
+
+            }
+
         }
     }
 }
