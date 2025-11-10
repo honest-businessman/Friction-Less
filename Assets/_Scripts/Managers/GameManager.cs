76869dc64e48d634e896f212755f503bd867c86c
Keisuke-Suzuki229 <154766861+Keisuke-Suzuki229@users.noreply.github.com>
2025-11-10 18:17:17 +1300

    modify GameManager
    using System;

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using TMPro;
+using System;
 
 public class GameManager : MonoBehaviour
 {
